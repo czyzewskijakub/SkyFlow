f6fd243a3aeb3f3c4a02250ed5d67114caf77729
Jakub Czyzewski <jakub.czyzewski@tomtom.com>
2023-05-07 00:22:43 +0200

    Change maven build goal.

--- src/test/java/pl/ioad/skyflow/RoutesTest.java
+++ src/test/java/pl/ioad/skyflow/RoutesTest.java
@@ -1,20 +1,15 @@
 package pl.ioad.skyflow;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @WebFluxTest
-@ContextConfiguration(classes = Routes.class)
 class RoutesTest {
-
-    @Autowired
-    Routes routes;
 
     @Test
     void getAllFlights() {
+        Routes routes = new Routes();
         WebTestClient client = WebTestClient.bindToRouterFunction(routes.getAllFlights())
                 .build();
 
