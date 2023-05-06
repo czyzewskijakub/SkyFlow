484c5aa40a48388b63e6327fc6a98c2ab5701990
Jakub Czyzewski <jakub.czyzewski@tomtom.com>
2023-05-07 00:13:22 +0200

    Add sample endpoint.

--- src/test/java/pl/ioad/skyflow/RoutesTest.java
+++ src/test/java/pl/ioad/skyflow/RoutesTest.java
@@ -1,26 +1,21 @@
 package pl.ioad.skyflow;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @WebFluxTest
-@Import({RoutesConfiguration.class, FlightsHandler.class})
+@ContextConfiguration(classes = Routes.class)
 class RoutesTest {
 
     @Autowired
-    RoutesConfiguration routes;
-
-    @Autowired
-    FlightsHandler handler;
+    Routes routes;
 
     @Test
-    @Disabled
     void getAllFlights() {
-        WebTestClient client = WebTestClient.bindToRouterFunction(routes.getAllFlights(handler))
+        WebTestClient client = WebTestClient.bindToRouterFunction(routes.getAllFlights())
                 .build();
 
         String expectedMessage = "All flights here";
