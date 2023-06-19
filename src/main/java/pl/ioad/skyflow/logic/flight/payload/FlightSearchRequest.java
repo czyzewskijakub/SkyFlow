eafa2c981d1e32aabc3032d8f7ba7ea84f47b7ee
Jakub Czyzewski <jakub.czyzewski@tomtom.com>
2023-06-19 23:16:46 +0200

    Improve flights search (#13)
    * Swagger configuration moved to separate file, admin registration method doc
    
    * User update method, Responses, Exceptions
    
    * User logic endpoints API documentation
    
    * Bearer token auth into swagger ui, only logged-in user can change this user's data
    
    * User with id DTO, change account type method, retrieve all users method
    
    * User tests, deleted UserWithIdDto class
    
    * API methods documentation corrected
    
    * Static imports, minor corrections
    
    * Convert epoch time to date time; Get flights from previous year.
    
    ---------
    
    Co-authored-by: matkrasinski <mat.krasinski@o2.pl>

--- src/main/java/pl/ioad/skyflow/logic/flight/payload/FlightSearchRequest.java
+++ src/main/java/pl/ioad/skyflow/logic/flight/payload/FlightSearchRequest.java
@@ -1,7 +1,33 @@
 package pl.ioad.skyflow.logic.flight.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import static pl.ioad.skyflow.logic.utils.TimeUtils.subtractDateTime;
+import static pl.ioad.skyflow.logic.utils.TimeUtils.toUnixTime;
+
 public record FlightSearchRequest(
+        @Schema(description = "ICAO code of airport", example = "EDDF")
+        @NotBlank
         String departureAirport,
-        Integer begin,
-        Integer end
-) { }
+        @Schema(description = "begin date", example = "2023-06-17 00:00:00")
+        @DateTimeFormat(pattern = "yyyy-MM-dd HH-mm-ss")
+        @NotBlank
+        String begin,
+        @Schema(description = "begin date", example = "2023-06-17 23:59:59")
+        @DateTimeFormat(pattern = "yyyy-MM-dd HH-mm-ss")
+        @NotBlank
+        String end) {
+
+    public FlightSearchRequest(String departureAirport, String begin, String end) {
+        begin = subtractDateTime(begin);
+        begin = toUnixTime(begin);
+        end = subtractDateTime(end);
+        end = toUnixTime(end);
+
+        this.departureAirport = departureAirport;
+        this.begin = begin;
+        this.end = end;
+    }
+}
---
+++ src/main/java/pl/ioad/skyflow/logic/utils/TimeUtils.java
@@ -0,0 +1,32 @@
+package pl.ioad.skyflow.logic.utils;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import static java.time.ZoneOffset.UTC;
+
+public abstract class TimeUtils {
+
+    public static String toUnixTime(String dateTime) {
+        var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        var localDateTime = LocalDateTime.parse(dateTime, formatter);
+        return String.valueOf(localDateTime.toEpochSecond(UTC));
+    }
+
+    public static String toDateTime(String unixTime) {
+        var date = new Date(Long.parseLong(unixTime) * 1000);
+        var sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sdf.format(date);
+    }
+
+    public static String addDateTime(String dateTime) {
+        return (Integer.parseInt(dateTime.substring(0, 4)) + 1) + "-" + dateTime.substring(5);
+    }
+
+    public static String subtractDateTime(String dateTime) {
+        return (Integer.parseInt(dateTime.substring(0, 4)) - 1) + "-" + dateTime.substring(5);
+    }
+
+}
