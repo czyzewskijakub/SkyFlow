c61b071e5ed617af0a3379e54b271555068b57c5
MichalAndrzejczak1 <236489@edu.p.lodz.pl>
2023-06-20 11:41:33 +0200

    Reservation endpoints documentation

--- src/main/java/pl/ioad/skyflow/logic/reservation/ReservationController.java
+++ src/main/java/pl/ioad/skyflow/logic/reservation/ReservationController.java
@@ -2,6 +2,8 @@ package pl.ioad.skyflow.logic.reservation;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpServletRequest;
@@ -25,11 +27,54 @@ public class ReservationController {
 
     @Operation(summary = "Book a flight")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully booked flight"),
-            @ApiResponse(responseCode = "404", description = "Flight not found"),
-            @ApiResponse(responseCode = "403", description = "You cannot perform the operation"),
-            @ApiResponse(responseCode = "401", description = "You are not authorized to book a flight"),
-            @ApiResponse(responseCode = "400", description = "Not correct request")
+            @ApiResponse(responseCode = "200", description = "Successfully booked flight", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "message": "Successfully booked flight"
+                                                }
+                                                """
+                    ))
+            ),
+            @ApiResponse(responseCode = "400", description = "Not correct request", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 400,
+                                                  "exception": "Exception",
+                                                  "message": "Bad request"
+                                                }
+                                                """
+                    ))
+            ),
+            @ApiResponse(responseCode = "401", description = "You are not authorized to book a flight", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 401,
+                                                  "exception": "Exception",
+                                                  "message": "Unauthorized"
+                                                }
+                                                """
+                    ))
+            ),
+            @ApiResponse(responseCode = "403", description = "You cannot perform the operation", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 403,
+                                                  "exception": "Exception",
+                                                  "message": "Forbidden"
+                                                }
+                                                """
+                    ))
+            ),
+            @ApiResponse(responseCode = "404", description = "Flight not found", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 404,
+                                                  "exception": "Exception",
+                                                  "message": "Not found"
+                                                }
+                                                """
+                    ))
+            )
     })
     @PostMapping("/book")
     public ResponseEntity<ReservationResponse> bookFlight(
@@ -43,11 +88,54 @@ public class ReservationController {
 
     @Operation(summary = "Cancel a flight reservation")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully cancelled flight"),
-            @ApiResponse(responseCode = "404", description = "Flight not found"),
-            @ApiResponse(responseCode = "403", description = "You cannot perform the operation"),
-            @ApiResponse(responseCode = "401", description = "You are not authorized to cancel a flight"),
-            @ApiResponse(responseCode = "400", description = "Not correct request")
+            @ApiResponse(responseCode = "200", description = "Successfully cancelled flight", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "message": "Successfully canceled flight"
+                                                }
+                                                """
+                    ))
+            ),
+            @ApiResponse(responseCode = "400", description = "Not correct request", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 400,
+                                                  "exception": "Exception",
+                                                  "message": "Bad request"
+                                                }
+                                                """
+                    ))
+            ),
+            @ApiResponse(responseCode = "401", description = "You are not authorized to cancel a flight", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 401,
+                                                  "exception": "Exception",
+                                                  "message": "Unauthorized"
+                                                }
+                                                """
+                    ))
+            ),
+            @ApiResponse(responseCode = "403", description = "You cannot perform the operation", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 403,
+                                                  "exception": "Exception",
+                                                  "message": "Forbidden"
+                                                }
+                                                """
+                    ))
+            ),
+            @ApiResponse(responseCode = "404", description = "Flight not found", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 404,
+                                                  "exception": "Exception",
+                                                  "message": "Not found"
+                                                }
+                                                """
+                    ))
+            ),
     })
     @PostMapping("/cancel")
     public ResponseEntity<ReservationResponse> cancelReservation(
@@ -58,13 +146,58 @@ public class ReservationController {
         return ResponseEntity.ok().body(reservationService.cancelFlight(request, httpServletRequest));
     }
 
-    @Operation(summary = "Cancel a flight reservation")
+    @Operation(summary = "Get your reservations")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully cancelled flight"),
-            @ApiResponse(responseCode = "404", description = "Flight not found"),
-            @ApiResponse(responseCode = "403", description = "You cannot perform the operation"),
-            @ApiResponse(responseCode = "401", description = "You are not authorized to cancel a flight"),
-            @ApiResponse(responseCode = "400", description = "Not correct request")
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved user flights", content = @Content(
+                    schema = @Schema(example = """
+                                                [
+                                                  {
+                                                    "reservationId": 2
+                                                  }
+                                                ]
+                                                """
+                    ))
+            ),
+            @ApiResponse(responseCode = "400", description = "Not correct request", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 400,
+                                                  "exception": "Exception",
+                                                  "message": "Bad request"
+                                                }
+                                                """
+                    ))
+            ),
+            @ApiResponse(responseCode = "401", description = "You are not authorized to cancel a flight", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 401,
+                                                  "exception": "Exception",
+                                                  "message": "Unauthorized"
+                                                }
+                                                """
+                    ))
+            ),
+            @ApiResponse(responseCode = "403", description = "You cannot perform the operation", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 403,
+                                                  "exception": "Exception",
+                                                  "message": "Forbidden"
+                                                }
+                                                """
+                    ))
+            ),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 404,
+                                                  "exception": "Exception",
+                                                  "message": "Not found"
+                                                }
+                                                """
+                    ))
+            )
     })
     @GetMapping("/reservations")
     public ResponseEntity<List<ReservationDTO>> getUserReservation(@Parameter(description = "HTTP Servlet Request",
