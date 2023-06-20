23acab357ee1ba3bea574ef92ff03a0bd6b6ebf8
KamilPresler <kamilpfcb@gmail.com>
2023-06-20 20:29:52 +0200

    test

--- src/main/java/pl/ioad/skyflow/logic/paypal/PayPalController.java
+++ src/main/java/pl/ioad/skyflow/logic/paypal/PayPalController.java
@@ -1,6 +1,5 @@
 package pl.ioad.skyflow.logic.paypal;
 
-import com.paypal.api.payments.Links;
 import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.PayPalRESTException;
 import jakarta.persistence.EntityNotFoundException;
@@ -10,12 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import pl.ioad.skyflow.database.model.User;
 import pl.ioad.skyflow.database.repository.CartRepository;
 import pl.ioad.skyflow.database.repository.UserRepository;
 
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,10 +22,6 @@ public class PayPalController {
     private final UserRepository userRepository;
     private final CartRepository cartRepository;
 
-    public static final String URL = "http://localhost:8080";
-    public static final String SUCCESS_URL = "/success";
-    public static final String CANCEL_URL = "/fail";
-    public static final String CURRENCY = "EUR";
 
 
     @GetMapping("/payment")
@@ -43,42 +35,19 @@ public class PayPalController {
     @PostMapping("/pay")
     public ResponseEntity<String> payment(@RequestParam Long userId) throws PayPalRESTException {
         try {
-            Optional<User> user = userRepository.findById(userId);
-            if (user.isEmpty()) {
-                throw new EntityNotFoundException("User not found");
-            }
-            double totalPrice = cartRepository.findAll().stream().filter(ticket ->
-                            ticket.getUser() == user.get())
-                    .mapToDouble(cart -> cart.getTicket().getPrice()).sum();
-            if (totalPrice == 0) {
-                throw new EntityNotFoundException("Total price is equal to 0.");
-            }
-
-            Payment payment = service.createPayment(
-                    totalPrice,
-                    CURRENCY,
-                    URL + CANCEL_URL,
-                    URL + SUCCESS_URL);
-            for (Links link : payment.getLinks()) {
-                if (link.getRel().equals("approval_url")) {
-                    String test = link.getHref();
-                    System.out.println(test);
-                    test = test.replace("https://", "");
-                    return ResponseEntity.ok(test);
-                }
-            }
+            service.preparePayment(userId);
         } catch (PayPalRESTException e) {
             throw new PayPalRESTException("Payment fail");
         }
         return ResponseEntity.ok("");
     }
 
-    @GetMapping(value = CANCEL_URL)
+    @GetMapping("/fail")
     public String cancelPay() {
         return "cancel.html";
     }
 
-    @GetMapping(value = SUCCESS_URL)
+    @GetMapping("/success")
     public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) throws PayPalRESTException {
         try {
             Payment payment = service.executePayment(paymentId, payerId);
--- src/main/java/pl/ioad/skyflow/logic/paypal/PayPalService.java
+++ src/main/java/pl/ioad/skyflow/logic/paypal/PayPalService.java
@@ -3,50 +3,81 @@ package pl.ioad.skyflow.logic.paypal;
 import com.paypal.api.payments.*;
 import com.paypal.base.rest.APIContext;
 import com.paypal.base.rest.PayPalRESTException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import pl.ioad.skyflow.database.model.User;
+import pl.ioad.skyflow.database.repository.CartRepository;
+import pl.ioad.skyflow.database.repository.UserRepository;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class PayPalService {
     public static final String PAYMENT_METHOD = "paypal";
     public static final String INTENT = "sale";
-    @Autowired
-    private APIContext apiContext;
+
+    private static final String URL = "http://localhost:8080";
+
+    private static final String CURRENCY = "EUR";
+
+    private final UserRepository userRepository;
+    private final CartRepository cartRepository;
+
+    public static final String SUCCESS_URL = "/success";
+    public static final String CANCEL_URL = "/fail";
+
+    private final APIContext apiContext;
 
     public Payment createPayment(
             Double total,
             String currency,
             String cancelUrl,
             String successUrl) throws PayPalRESTException {
+        Payment payment = new Payment();
+        payment.setIntent(INTENT);
+        payment.setPayer(getPayer());
+        payment.setTransactions(getTransactionList(getAmount(currency, total)));
+        RedirectUrls redirectUrls = new RedirectUrls();
+        redirectUrls.setCancelUrl(cancelUrl);
+        redirectUrls.setReturnUrl(successUrl);
+        payment.setRedirectUrls(redirectUrls);
+        return payment.create(apiContext);
+    }
+
+    private Amount getAmount(String currency, Double total) {
         Amount amount = new Amount();
         amount.setCurrency(currency);
         total = new BigDecimal(total).setScale(2, RoundingMode.HALF_UP).doubleValue();
         DecimalFormat df = new DecimalFormat("#.##");
         amount.setTotal(df.format(total));
+        return amount;
+    }
+
+    private List<Transaction> getTransactionList(Amount amount) {
         Transaction transaction = new Transaction();
-        transaction.setDescription("Test");
         transaction.setAmount(amount);
+
         List<Transaction> transactions = new ArrayList<>();
         transactions.add(transaction);
+
+        return transactions;
+    }
+
+    private Payer getPayer() {
         Payer payer = new Payer();
         payer.setPaymentMethod(PAYMENT_METHOD);
-        Payment payment = new Payment();
-        payment.setIntent(INTENT);
-        payment.setPayer(payer);
-        payment.setTransactions(transactions);
-        RedirectUrls redirectUrls = new RedirectUrls();
-        redirectUrls.setCancelUrl(cancelUrl);
-        redirectUrls.setReturnUrl(successUrl);
-        payment.setRedirectUrls(redirectUrls);
-
-        return payment.create(apiContext);
+        return payer;
     }
+
 
     public Payment executePayment(String paymentId, String payerId) throws PayPalRESTException {
         Payment payment = new Payment();
@@ -55,4 +86,34 @@ public class PayPalService {
         paymentExecution.setPayerId(payerId);
         return payment.execute(apiContext, paymentExecution);
     }
+
+    public String preparePayment(Long userId) throws PayPalRESTException {
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isEmpty()) {
+            throw new EntityNotFoundException("User not found");
+        }
+        double totalPrice = cartRepository.findAll().stream().filter(ticket ->
+                        ticket.getUser() == user.get())
+                .mapToDouble(cart -> cart.getTicket().getPrice()).sum();
+        if (totalPrice == 0) {
+            throw new EntityNotFoundException("Total price is equal to 0.");
+        }
+
+        Payment payment = createPayment(
+                totalPrice,
+                CURRENCY,
+                URL + CANCEL_URL,
+                URL + SUCCESS_URL);
+        for (Links link : payment.getLinks()) {
+            if (link.getRel().equals("approval_url")) {
+                String test = link.getHref();
+                System.out.println(test);
+                test = test.replace("https://", "");
+                return test;
+            }
+        }
+        return "";
+    }
+
+
 }
