b04b635a8685a584013209334b563b9f07a2cbbd
Mateusz Krasiński <mat.krasinski@o2.pl>
2023-06-20 18:15:16 +0200

    MethodArgumentNotValidException handler (#29)

--- src/main/java/pl/ioad/skyflow/logic/user/UserService.java
+++ src/main/java/pl/ioad/skyflow/logic/user/UserService.java
@@ -45,15 +45,16 @@ public class UserService {
         if (userRepository.existsByEmail(request.getEmail())) {
             throw new ForbiddenException("Email is taken");
         }
-
-        User user = userRepository.save(User.builder()
+        User user = User.builder()
                 .firstName(request.getFirstName())
                 .lastName(request.getLastName())
                 .email(request.getEmail())
                 .passwordHash(encoder.encode(request.getPassword()))
                 .profilePictureUrl(request.getPictureUrl())
                 .isAdmin(false)
-                .build());
+                .build();
+        userRepository.save(user);
+
         return new UserResponse(
                 CREATED.value(),
                 "Successfully registered user account",
