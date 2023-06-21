a1ba08b80e2c3ca6d2e8cea6e805430d050cce41
Ryszawy <Krafi604@gmail.com>
2023-06-21 10:27:06 +0200

    RestController, change ticket status

--- src/main/java/pl/ioad/skyflow/logic/paypal/PayPalController.java
+++ src/main/java/pl/ioad/skyflow/logic/paypal/PayPalController.java
@@ -5,15 +5,13 @@ import com.paypal.base.rest.PayPalRESTException;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import pl.ioad.skyflow.database.repository.UserRepository;
 
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/paypal")
 public class PayPalController {
 
 
