70ee66087d93749e8897b8c391d56e63b95cb324
Jakub Czyzewski <jakub.czyzewski@tomtom.com>
2023-06-21 17:28:35 +0200

    Fix test for user registration.

--- src/test/java/pl/ioad/skyflow/logic/flight/FlightControllerTest.java
+++ src/test/java/pl/ioad/skyflow/logic/flight/FlightControllerTest.java
@@ -1,31 +1,24 @@
 package pl.ioad.skyflow.logic.flight;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import pl.ioad.skyflow.TestUtils;
 import pl.ioad.skyflow.logic.flight.payload.FlightSearchRequest;
-import pl.ioad.skyflow.logic.user.payload.request.LoginRequest;
-import pl.ioad.skyflow.logic.user.payload.response.AuthorizationResponse;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @SpringBootTest
 class FlightControllerTest {
-
-    private static final LoginRequest CREDENTIALS = new LoginRequest(
-            "example@gmail.com",
-            "123abC#"
-    );
 
     private static final FlightSearchRequest FLIGHT_SEARCH_REQUEST = new FlightSearchRequest(
             "EDDF",
@@ -43,11 +36,13 @@ class FlightControllerTest {
 
     private MockMvc mockMvc;
     private String token;
+    private TestUtils utils;
 
     @BeforeEach
     public void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
-        this.token = login();
+        this.mockMvc = webAppContextSetup(this.webApplicationContext).build();
+        this.utils = new TestUtils(this.mockMvc, this.mapper);
+        this.token = this.utils.login();
     }
 
     @Test
@@ -55,33 +50,10 @@ class FlightControllerTest {
         var request = post(UPCOMING_FLIGHTS_ENDPOINT)
                 .header("Authorization", this.token)
                 .contentType(APPLICATION_JSON)
-                .content(mockRequestBody(FLIGHT_SEARCH_REQUEST));
+                .content(this.utils.mockRequestBody(FLIGHT_SEARCH_REQUEST));
         this.mockMvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(APPLICATION_JSON))
                 .andExpect(jsonPath("$.length()").value(44));
-    }
-
-    private String login() {
-        var request = post("/users/login")
-                .contentType(APPLICATION_JSON)
-                .content(mockRequestBody(CREDENTIALS));
-        try {
-            var response = this.mockMvc.perform(request)
-                    .andReturn()
-                    .getResponse()
-                    .getContentAsString();
-            return "Bearer " + mapper.readValue(response, AuthorizationResponse.class).getToken();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private String mockRequestBody(final Object requestBody) {
-        try {
-            return mapper.writeValueAsString(requestBody);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
--- src/test/java/pl/ioad/skyflow/logic/user/UserServiceTest.java
+++ src/test/java/pl/ioad/skyflow/logic/user/UserServiceTest.java
@@ -1,69 +1,95 @@
 package pl.ioad.skyflow.logic.user;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import pl.ioad.skyflow.database.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.context.WebApplicationContext;
+import pl.ioad.skyflow.TestUtils;
 import pl.ioad.skyflow.database.repository.UserRepository;
 import pl.ioad.skyflow.logic.user.payload.request.UserDataRequest;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.allOf;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
-
+@SpringBootTest
+@Transactional
 public class UserServiceTest {
 
-    @InjectMocks
+    @Autowired
     private UserService userService;
-    @Mock
+    @Autowired
     private UserRepository userRepository;
-    @Mock
-    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Autowired
+    private ObjectMapper mapper;
+
+    private static final String REGISTER_USER_ENDPOINT = "/users/register";
+
+    private MockMvc mockMvc;
+    private String token;
+    private TestUtils utils;
+
+    // Test data
+    private final String firstName = "John";
+    private final String lastName = "Smith";
+    private final String email = "johnsmith@gmail.com";
+    private final String pictureUrl = "https://pl.pinterest.com/pin/327848047887112192/";
+    private final String password = "password";
 
     @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @Disabled
-    @Test
-    void testRegisterUser_Success() {
-        String firstName = "John";
-        String lastName = "Smith";
-        String email = "johnsmith@gmail.com";
-        String pictureUrl = "https://pl.pinterest.com/pin/327848047887112192/";
-        String password = "password";
-
-        User user = new User(1L, firstName, lastName, email, pictureUrl, password, false);
-
-        UserDataRequest userData = new UserDataRequest(firstName, lastName, email, pictureUrl, password);
-
-        when(userRepository.save(any(User.class))).thenReturn(user);
-        when(passwordEncoder.encode(anyString())).thenReturn("encodedPassword");
-
-        var registeredUser = userService.register(userData).getUser();
-
-        verify(userRepository, times(1)).existsByEmail(anyString());
-        verify(userRepository, times(1)).save(any(User.class));
-        verify(passwordEncoder, times(1)).encode(anyString());
-
-        assertNotNull(registeredUser);
-        assertEquals(email, registeredUser.getEmail());
+    public void setup() {
+        this.mockMvc = webAppContextSetup(this.webApplicationContext).build();
+        this.utils = new TestUtils(this.mockMvc, this.mapper);
+        this.token = this.utils.login();
     }
 
     @Test
-    void testRegisterUser_Failure() {
-        String firstName = "John";
-        String lastName = "Smith";
-        String pictureUrl = "https://pl.pinterest.com/pin/327848047887112192/";
-        String password = "password";
+    void testRegisterUserAndDelete_Success() throws Exception {
+
+        UserDataRequest userData = UserDataRequest.builder()
+                .firstName(firstName)
+                .lastName(lastName)
+                .email(email)
+                .password(password)
+                .pictureUrl(pictureUrl)
+                .build();
+
+        var request = post(REGISTER_USER_ENDPOINT)
+                .header("Authorization", this.token)
+                .contentType(APPLICATION_JSON)
+                .content(this.utils.mockRequestBody(userData));
+        this.mockMvc.perform(request)
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(APPLICATION_JSON));
+
+        assertThat(userRepository.findByEmail(email))
+                .isPresent()
+                .get()
+                .has(allOf(
+                        new Condition<>(u -> u.getFirstName().equals(firstName), "first name"),
+                        new Condition<>(u -> u.getLastName().equals(lastName), "last name"),
+                        new Condition<>(u -> u.getProfilePictureUrl().equals(pictureUrl), "picture url"),
+                        new Condition<>(u -> !u.getPasswordHash().equals(password), "password hash")
+                ));
+
+        userService.deleteUserByEmail(email);
+    }
+
+    @Test
+    void testRegisterUser_Failure() throws Exception {
 
         UserDataRequest userData = UserDataRequest.builder()
                 .firstName(firstName)
@@ -72,9 +98,14 @@ public class UserServiceTest {
                 .pictureUrl(pictureUrl)
                 .build();
 
-        assertThrows(NullPointerException.class, () -> userService.register(userData));
+        var request = post(REGISTER_USER_ENDPOINT)
+                .header("Authorization", this.token)
+                .contentType(APPLICATION_JSON)
+                .content(this.utils.mockRequestBody(userData));
+        this.mockMvc.perform(request)
+                .andExpect(result -> assertThat(result.getResolvedException()).isInstanceOf(MethodArgumentNotValidException.class));
 
-        verify(userRepository, never()).existsByEmail(anyString());
+        assertThat(userRepository.existsByEmail(email)).isFalse();
     }
 
 }
