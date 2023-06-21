ead9496478ef7afb01f82427035caaaa3d589dbf
Ryszawy <Krafi604@gmail.com>
2023-06-21 16:09:50 +0200

    PayPal swagger description

--- src/main/java/pl/ioad/skyflow/logic/paypal/PayPalController.java
+++ src/main/java/pl/ioad/skyflow/logic/paypal/PayPalController.java
@@ -2,6 +2,11 @@ package pl.ioad.skyflow.logic.paypal;
 
 import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.PayPalRESTException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +23,6 @@ public class PayPalController {
     private final PayPalService service;
     private final UserRepository userRepository;
 
-
     @GetMapping("/payment")
     public String home(@RequestParam Long userId) {
         if (userRepository.findById(userId).isEmpty()) {
@@ -26,6 +30,48 @@ public class PayPalController {
         }
         return "home.html";
     }
+
+    @Operation(summary = "Pay for flight")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully redirect to PayPal page", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                    "message": "Successfully redirect to PayPal page"
+                                                }
+                                                """
+                    )
+            )),
+            @ApiResponse(responseCode = "400", description = "Not correct request", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 400,
+                                                  "exception": "Exception",
+                                                  "message": "Bad request"
+                                                }
+                                                """
+                    )
+            )),
+            @ApiResponse(responseCode = "401", description = "You are not authorized to pay for flight", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 401,
+                                                  "exception": "Exception",
+                                                  "message": "Unauthorized"
+                                                }
+                                                """
+                    )
+            )),
+            @ApiResponse(responseCode = "403", description = "You cannot perform the operation", content = @Content(
+                    schema = @Schema(example = """
+                                                {
+                                                  "httpStatus": 403,
+                                                  "exception": "Exception",
+                                                  "message": "Forbidden"
+                                                }
+                                                """
+                    )
+            )),
+    })
 
     @PostMapping("/pay")
     public ResponseEntity<String> payment(@RequestParam Long userId) throws PayPalRESTException {
